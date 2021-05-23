39a66e8427df922a1067f09b73563c28dd1d3214
inventerlord <inventerlord@gmail.com>
2021-05-23 21:27:35 +0430

    react
    React

---
+++ crwn-clothing/src/components/homePage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+function HomePage(){
+    <div className={'homepage'}>
+        <div className={'category-menu'}>
+            <div className={'menu-item'}>
+                <div className={'content'}>
+                    <h1 className={'title'}>HATS</h1>
+                    <span className={'subtitle'}></span>
+                </div>
+            </div>
+        </div>
+    </div>
+}
+
+export default HomePage;
